3ec36268b31c3d8acdc1e891e992ab6e427fe2a8
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-06 19:46:33 +0400

    Added initial function save to Local Storage (save only taskName)

--- index.js
+++ index.js
@@ -197,3 +197,18 @@ filterButtons.forEach((button) => {
       });
     });
   });
+
+
+
+//   Save Todo LocalStorge
+function saveTodo(todo){
+    let todos;
+    if(localStorage.getItem("todos") === null){
+      todos = [];
+    }else{
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+  
+    todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
