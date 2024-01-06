30dc2818343651e0edefef3cbe7c404beae34775
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-06 19:08:19 +0400

    adding initial selectors for further work

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Selectors
+const taskTitle = document.querySelector("#taskTitle");
+const taskDescription = document.querySelector("#taskDescription");
+const addBtn = document.querySelector(".add");
+const taskList = document.querySelector("#taskList");
+const filterButtons = document.querySelectorAll(".filter-btns button");
+
+
 class Task{
     constructor(){};
 }
