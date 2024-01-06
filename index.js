88dccc7843f813429cf5e48dd61a68788abf01b8
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-06 19:13:49 +0400

    Change createDate on function and created function  createDAteTime

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ class TaskList{
         this.#id = Math.random().toString(16).slice(2);
         this.#title = title;
         this.#description = description;
-        this.#createDate = createDate;
+        this.#createDate = this.getCurrentDateTime();
         this.#isCompleted = false;
       }
+
+      getCurrentDateTime() {
+        const now = new Date();
+        const day = String(now.getDate()).padStart(2, "0");
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const year = now.getFullYear();
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+        return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+      }
+    
 }
 
