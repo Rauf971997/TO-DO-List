49a9e70c7ffa85296a89c60f8ad2f89e8cc9f48a
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-06 19:31:56 +0400

    Added new method for added tasks on main page

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ class Task{
       }
 }
 
+const task=new Task();
 
 // class Task List
 class TaskList{
@@ -73,6 +74,60 @@ class TaskList{
     const descriptionRegex = /^(?!\s+$)([a-zA-Z]{1,16}|[а-яА-Я]{1,16}|\d{1,16})(\s([a-zA-Z]{1,16}|[а-яА-Я]{1,16}|\d{1,16})){0,15}\s*$/u;
     return titleRegex.test(title) && descriptionRegex.test(description) && description.trim() !== title.trim();
   }
+  //Validation form end
+
+  addTodo = (e) => {
+    
+    const taskTitleValue = taskTitle.value;
+    const taskDescriptionValue = taskDescription.value;
+
+    // Validation process
+    if (this.validationForm(taskTitleValue,taskDescriptionValue)) {
+      // Create div 
+      const todoDiv = document.createElement("div");
+      todoDiv.classList.add("todo");
+
+      // Create LI with a link
+      const newTodo = document.createElement("li");
+      const todoLink = document.createElement("a");
+      todoLink.href = `src/page/details.html?id=${encodeURIComponent(task.getId())}`; //For checkout me
+      todoLink.textContent = taskTitle.value; 
+      newTodo.classList.add("todo-item");
+      newTodo.appendChild(todoLink);
+      todoDiv.appendChild(newTodo);
+
+  
+
+      // Check mark button
+      const completedButton = document.createElement("button");
+      completedButton.innerHTML = '<i class="fas fa-check"></i>';
+      completedButton.classList.add("complete-btn");
+      todoDiv.appendChild(completedButton);
+
+      // Check Delete Button
+      const trashButton = document.createElement("button");
+      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+      trashButton.classList.add("trash-btn");
+      todoDiv.appendChild(trashButton);
+
+      // Edit Button
+      const editBtn = document.createElement("button");
+      editBtn.innerHTML = '<i class="fas fa-edit"></i>';
+      editBtn.classList.add("edit-btn");
+      todoDiv.appendChild(editBtn);
+    
+      // Append to list
+      taskList.appendChild(todoDiv); 
+
+      // Clear Todo InputValue
+      taskTitle.value = "";
+      taskDescription.value = ""; // Очистить поле с описанием задачи после добавления
+    }
+    else {
+      alert('Incorrect task name. Please follow the entry rules.');
+    }
+
+  };
 
 }
 
