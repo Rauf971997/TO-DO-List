f0a9bf77492040a22038c55e52da4ded1331dbaf
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-10 18:19:15 +0400

    Added functions for the correct operation of the page with a detailed description

---
+++ src/js/details.js
@@ -0,0 +1,35 @@
+document.querySelector(".toHome").addEventListener("click", function() {
+    window.location.href = "../../index.html"; // или window.location.replace("../../index.html");
+  });
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const taskId = urlParams.get('id'); ge
+    const storedData = localStorage.getItem('todos');
+    const todos = JSON.parse(storedData) || []; 
+  
+    const taskDetails = todos.find(task => task.id === taskId);
+  
+    if (taskDetails) {
+      displayTaskDetails(taskDetails);
+    } else {
+      alert('Oops, 404 Error: Task not found');
+    }
+  });
+  
+ 
+  function displayTaskDetails(taskDetails) {
+    const taskIdElement = document.getElementById('taskId');
+    const taskTitleElement = document.getElementById('taskTitle');
+    const taskDescriptionElement = document.getElementById('taskDescription');
+    const taskCreateDateElement = document.getElementById('taskCreateDate');
+    const taskStatusElement = document.getElementById('taskStatus');
+  
+    taskIdElement.textContent = taskDetails.id;
+    taskTitleElement.textContent = taskDetails.title;
+    taskDescriptionElement.textContent = taskDetails.description;
+    taskCreateDateElement.textContent = taskDetails.createDate;
+    taskStatusElement.textContent = taskDetails.isCompleted ? 'Completed' : 'Incomplete';
+    
+  }
+  
