c70ca8bd17e382ff8a65a81f381cf7ab84ae21e1
Baghirov Rauf <rauf777bagirov7@gmail.com>
2024-01-15 12:28:00 +0400

    removed extra spaces in the file

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const addBtn = document.querySelector(".add");
 const taskList = document.querySelector("#taskList");
 const filterButtons = document.querySelectorAll(".filter-btns button");
 
-
 class Task {
   #id;
   #title;
@@ -32,16 +31,14 @@ class Task {
     return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
   }
 
-
   toObject() {
     return {
       title: this.#title,
       description: this.#description,
       createDate: this.#createDate,
-      isCompleted: this.#isCompleted
+      isCompleted: this.#isCompleted,
     };
   }
-
 
   getId() {
     return this.#id;
@@ -62,47 +59,48 @@ class Task {
   getIsCompleted() {
     return this.#isCompleted;
   }
-
 }
-
 
 // Class Task LIst
 class TaskList {
   #tasks = [];
 
-
   validationForm(title, description) {
-    const titleRegex = /^(?!\s)(?![\d\s]+$)([a-zA-Z]{1,16}|[а-яА-ЯёЁ]{1,16}|\d{1,16})(\s([a-zA-Z]{1,16}|[а-яА-ЯёЁ]{1,16}|\d{1,16})){1,15}(?<!\s)$/u;
-    const descriptionRegex = /^(?!\s+$)([a-zA-Z\s]{1,16}|[а-яА-ЯёЁ\s]{1,16}|\d{1,16})(\s([a-zA-Z\s]{1,16}|[а-яА-ЯёЁ\s]{1,16}|\d{1,16})){0,15}\s*$/u;
-    return titleRegex.test(title) && descriptionRegex.test(description) && description.trim() !== title.trim();
+    const titleRegex =
+      /^(?!\s)(?![\d\s]+$)([a-zA-Z]{1,16}|[а-яА-ЯёЁ]{1,16}|\d{1,16})(\s([a-zA-Z]{1,16}|[а-яА-ЯёЁ]{1,16}|\d{1,16})){1,15}(?<!\s)$/u;
+    const descriptionRegex =
+      /^(?!\s+$)([a-zA-Z\s]{1,16}|[а-яА-ЯёЁ\s]{1,16}|\d{1,16})(\s([a-zA-Z\s]{1,16}|[а-яА-ЯёЁ\s]{1,16}|\d{1,16})){0,15}\s*$/u;
+    return (
+      titleRegex.test(title) &&
+      descriptionRegex.test(description) &&
+      description.trim() !== title.trim()
+    );
   }
-
 
   addTodo = (e) => {
     e.preventDefault();
     const taskTitleValue = taskTitle.value;
     const taskDescriptionValue = taskDescription.value;
 
- 
     //  Todo div
-    if (this.validationForm(taskTitleValue,taskDescriptionValue)) {
+    if (this.validationForm(taskTitleValue, taskDescriptionValue)) {
       const newTask = new Task(taskTitleValue, taskDescriptionValue);
-      const taskId = newTask.getId(); 
-      this.addTasks(newTask); 
-      updateTasks();   
+      const taskId = newTask.getId();
+      this.addTasks(newTask);
+      updateTasks();
 
-      const sortSelect = document.getElementById('sortSelect');
-      sortSelect.value = 'default';
+      const sortSelect = document.getElementById("sortSelect");
+      sortSelect.value = "default";
 
-      // Create div 
+      // Create div
       const todoDiv = document.createElement("div");
       todoDiv.classList.add("todo");
-      todoDiv.dataset.id = taskId; 
+      todoDiv.dataset.id = taskId;
 
       // Create LI with a link
       const newTodo = document.createElement("li");
       const todoLink = document.createElement("a");
-      todoLink.textContent = taskTitle.value; 
+      todoLink.textContent = taskTitle.value;
       newTodo.classList.add("todo-item");
       newTodo.appendChild(todoLink);
       todoDiv.appendChild(newTodo);
@@ -127,25 +125,21 @@ class TaskList {
       editBtn.innerHTML = '<i class="fas fa-edit"></i>';
       editBtn.classList.add("edit-btn");
       todoDiv.appendChild(editBtn);
-    
-      // Append to list
-      taskList.appendChild(todoDiv); 
 
-      // Clear Todo InputValue
+      taskList.appendChild(todoDiv);
+
       taskTitle.value = "";
-      taskDescription.value = ""; 
-    } 
-    
-    else {
-      alert('Incorrect task name. Please follow the entry rules!');
+      taskDescription.value = "";
+    } else {
+      alert("Incorrect task name. Please follow the entry rules!");
     }
-  
   };
 
-
   addTasks(...tasks) {
-    tasks.forEach(task => {
-      const taskExists = this.#tasks.find(existingTask => existingTask.getId() === task.getId());
+    tasks.forEach((task) => {
+      const taskExists = this.#tasks.find(
+        (existingTask) => existingTask.getId() === task.getId()
+      );
       if (!taskExists) {
         this.#tasks.push(task);
       }
@@ -153,74 +147,70 @@ class TaskList {
   }
 
   deleteTask(taskId) {
-    const taskIndex = this.#tasks.findIndex(task => task.getId() === taskId);
+    const taskIndex = this.#tasks.findIndex((task) => task.getId() === taskId);
 
     if (taskIndex !== -1) {
       this.#tasks.splice(taskIndex, 1);
-      console.log(this.#tasks)
     }
   }
-  
-    
+
   getTasks() {
     return [...this.#tasks];
   }
-
- 
 }
-
 
 const task = new TaskList();
 
 // Event Listener
 addBtn.addEventListener("click", task.addTodo);
 
-
-
 taskList.addEventListener("click", (e) => {
-  e.stopPropagation(); 
+  e.stopPropagation();
 
   const item = e.target;
   const todo = item.closest(".todo");
   const taskId = todo.dataset.id;
 
-  if (item.classList.contains("trash-btn") || item.classList.contains("fa-trash")) {
-    task.deleteTask(taskId); 
+  if (
+    item.classList.contains("trash-btn") ||
+    item.classList.contains("fa-trash")
+  ) {
+    task.deleteTask(taskId);
     removeLocalTodos(taskId);
     todo.remove();
   }
 
-  if (item.classList.contains("complete-btn") || item.classList.contains("fa-check")) {
+  if (
+    item.classList.contains("complete-btn") ||
+    item.classList.contains("fa-check")
+  ) {
     const isCompleted = todo.classList.toggle("completed");
     saveTodoStatus(taskId, isCompleted);
   }
 
-// Для кнопки редактирования
-if (item.classList.contains("edit-btn") || item.classList.contains("fa-edit")) {
-  const todo = item.closest(".todo");
-  const taskId = todo.dataset.id;
-  if (taskId) {
-    const editURL = `src/page/edit.html?id=${encodeURIComponent(taskId)}`;
-    window.location.href = editURL;
-  } else {
-    console.error('Task ID is missing or not found.');
+  if (
+    item.classList.contains("edit-btn") ||
+    item.classList.contains("fa-edit")
+  ) {
+    const todo = item.closest(".todo");
+    const taskId = todo.dataset.id;
+    if (taskId) {
+      const editURL = `src/page/edit.html?id=${encodeURIComponent(taskId)}`;
+      window.location.href = editURL;
+    } else {
+      console.error("Task ID is missing or not found.");
+    }
   }
-}
-  
 
-  // Для ссылок на подробности задачи
   const link = e.target.closest("a");
   if (link) {
     const taskId = link.closest(".todo").dataset.id;
-    window.location.href = `src/page/details.html?id=${encodeURIComponent(taskId)}`;
+    window.location.href = `src/page/details.html?id=${encodeURIComponent(
+      taskId
+    )}`;
   }
-
 });
 
-
-
-// //////LOcal Storage Function
-// Функция для изменения статуса выполнения задачи
 function saveTodoStatus(taskId, isCompleted) {
   let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -232,19 +222,14 @@ function saveTodoStatus(taskId, isCompleted) {
   }
 
   const todoDiv = document.querySelector(`[data-id="${taskId}"]`);
-    const completedButton = todoDiv.querySelector(".complete-btn");
+  const completedButton = todoDiv.querySelector(".complete-btn");
   if (isCompleted) {
-    completedButton.style.backgroundColor = "green"; 
+    completedButton.style.backgroundColor = "green";
   } else {
-    completedButton.style.backgroundColor = "red"; 
-  }  //Super Finally
-
+    completedButton.style.backgroundColor = "red";
+  }
 }
 
-
-
-
-// Функция для сохранения задачи в localStorage
 function saveTodo(taskObject) {
   let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -253,12 +238,10 @@ function saveTodo(taskObject) {
 }
 // ====================================
 
-
-
 function getTodos() {
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-  todos.forEach(function(task) {
+  todos.forEach(function (task) {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     todoDiv.dataset.id = task.id;
@@ -274,8 +257,6 @@ function getTodos() {
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
-
-    
 
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
@@ -291,13 +272,12 @@ function getTodos() {
       todoDiv.classList.add("completed");
       completedButton.style.backgroundColor = "green";
     } else {
-      completedButton.style.backgroundColor = "red"; 
+      completedButton.style.backgroundColor = "red";
     }
 
     taskList.appendChild(todoDiv);
   });
 }
-
 
 function removeLocalTodos(taskId) {
   let todos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -305,24 +285,18 @@ function removeLocalTodos(taskId) {
   const updatedTodos = todos.filter((todo) => todo.id !== taskId);
   localStorage.setItem("todos", JSON.stringify(updatedTodos));
 
-  task.deleteTask(taskId); 
-  
-  
-  taskList.innerHTML = ""; 
-  getTodos(); 
+  task.deleteTask(taskId);
+
+  taskList.innerHTML = "";
+  getTodos();
 }
 
-
-
-
-
-
-function updateTasks(sortBy = 'default', filter = 'all') {
+function updateTasks(sortBy = "default", filter = "all") {
   let todos = JSON.parse(localStorage.getItem("todos")) || [];
-  
-  if (filter !== 'all') {
-    todos = todos.filter(task => {
-      if (filter === 'completed') {
+
+  if (filter !== "all") {
+    todos = todos.filter((task) => {
+      if (filter === "completed") {
         return task.isCompleted;
       } else {
         return !task.isCompleted;
@@ -330,21 +304,20 @@ function updateTasks(sortBy = 'default', filter = 'all') {
     });
   }
 
-  if (sortBy === 'title') {
+  if (sortBy === "title") {
     todos.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (sortBy === 'date') {
+  } else if (sortBy === "date") {
     todos.sort((a, b) => new Date(b.createDate) - new Date(a.createDate));
   }
 
-  
-  if (sortBy === 'date') {
+  if (sortBy === "date") {
     todos.reverse();
   }
 
-  const taskList = document.getElementById('taskList');
+  const taskList = document.getElementById("taskList");
   taskList.innerHTML = "";
 
-  todos.forEach(function(task) {
+  todos.forEach(function (task) {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     todoDiv.dataset.id = task.id;
@@ -361,8 +334,6 @@ function updateTasks(sortBy = 'default', filter = 'all') {
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
-    
-
     const trashButton = document.createElement("button");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("trash-btn");
@@ -375,45 +346,37 @@ function updateTasks(sortBy = 'default', filter = 'all') {
 
     if (task.isCompleted) {
       todoDiv.classList.add("completed");
-      completedButton.style.backgroundColor = "green"; 
+      completedButton.style.backgroundColor = "green";
     } else {
-      completedButton.style.backgroundColor = "red"; 
+      completedButton.style.backgroundColor = "red";
     }
 
     taskList.appendChild(todoDiv);
   });
-
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  getTodos();
+  updateTasks();
+  const sortSelect = document.getElementById("sortSelect");
+  const filterButtons = document.querySelectorAll(".filter-btn");
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  getTodos(); 
-  updateTasks(); 
-  const sortSelect = document.getElementById('sortSelect');
-  const filterButtons = document.querySelectorAll('.filter-btn');
-
-  sortSelect.addEventListener('change', (event) => {
+  sortSelect.addEventListener("change", (event) => {
     const sortBy = event.target.value;
-    const filter = document.querySelector('.filter-btn.active').textContent.toLowerCase();
+    const filter = document
+      .querySelector(".filter-btn.active")
+      .textContent.toLowerCase();
     updateTasks(sortBy, filter);
   });
 
-  
-  
-  filterButtons.forEach(button => {
-    button.addEventListener('click', function () {
-      filterButtons.forEach(btn => btn.classList.remove('active'));
-      this.classList.add('active');
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      filterButtons.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
 
       const filter = this.textContent.toLowerCase();
-      sortSelect.value;
-      updateTasks( sortSelect.value, filter);   
+      sortSelect.value; //Checkout
+      updateTasks(sortSelect.value, filter);
     });
   });
 });
-
-
-
-
